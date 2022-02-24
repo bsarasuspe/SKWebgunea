1bf402b79a9dd5dbf697222ffa7ab950b3e204f6
bsarasuspe <83415989+bsarasuspe@users.noreply.github.com>
2022-02-24 10:12:54 +0100

    Zuhaitza konponduta

--- menu/tree_items.js
+++ menu/tree_items.js
@@ -1,23 +1,96 @@
 /* --- menu items --- */
-var TREE_ITEMS = 
-[
-	['KlinikApp proiektua', '../main/home_page.htm',
-			['Portada', '../main/home_page.htm'],		
-			['Aurkibide orokorra', '../Proyecto/Memoria/indice'],
-			['Aurkezpena', '../Proyecto/Aurkezpena.ppt'],
-			['Memoria','../Proyecto/Memoria/1_0_memoria.htm',
-				['01. Sarrera', '../Proyecto/Memoria/introduccion'],
-				['02. Helburua', '../Proyecto/Memoria/objeto'],
-    	                     ],
-                          
-			['Memoriaren eranskinak',null,
-				['Sarrerako dokumentazioa', '../Proyecto/Memoria-Anexos/1_documentacion-entrada.htm'],
-				['Sistemaren espezifikazioa','../Proyecto/Memoria-Anexos/2_especificacion-sistema.htm',
-					['Betekizunak', null,
-				         	['Glosategia', '../Proyecto/Memoria/1_0_memoria_archivos/rup_glossary.pdf'],
-				         	['Ikuspegia', '../Proyecto/Memoria/1_0_memoria_archivos/vision_tpl.pdf'],
-							['Betebeharren espezifikazioa', '../Proyecto/Memoria/1_0_memoria_archivos/systemwide_req_spec.pdf'],
-							['Erabilpen-Kasuak', null,
+var TREE_ITEMS = [
+
+	[' KlinikApp proiektua', '../main/home_page.htm',
+		[' Portada', '../main/home_page.htm'],
+		[' Laburpen Posterra', '../proiektua/4_posterra.htm'],
+		[' Aurkezpena', '../proiektua/LegardaJon-azkena.pptx'],
+		[' Bideo-tutoriala', '../proiektua/5_bideotutoriala.htm'],
+		[' Aurkibide orokorra', '../proiektua/1_memoria/0_aurkibidea.pdf'],				
+		[' Memoria', '../proiektua/0_osoa/1_memoria.pdf',
+			[' 1 Sarrera', '../proiektua/1_memoria/1_sarrera.pdf'],
+			[' 2 Proiektuaren Xedea', '../proiektua/1_memoria/2_proiektuaren_xedea.pdf'],
+			[' 3 Aurrekariak', '../proiektua/1_memoria/3_aurrekariak.pdf'],
+			[' 4 Egungo egoera', '../proiektua/1_memoria/4_egungo_egoera.pdf'],
+		    [' 5 Arauak eta erreferentziak', '../proiektua/1_memoria/5_arauak_erreferentziak.pdf'],
+			[' 6 Definizionak eta laburdurak', '../proiektua/1_memoria/6_definizioak_laburdurak.pdf'],
+			[' 7 Hasierako betekizunak', '../proiektua/1_memoria/7_hasierako_betekizunak.pdf'],
+			[' 8 Irismena', '../proiektua/1_memoria/8_irismena.pdf'],
+			[' 9 Hipotesiak eta Murriztapenak', '../proiektua/1_memoria/9_hipotesiak_murriztapenak.pdf'],
+			[' 10 Aukeren ikerketa eta egingarritasuna', '../proiektua/1_memoria/10_aukeren_ikerketa_egingarritasuna.pdf'],
+			[' 11 Proposatutako sistemaren deskribapena', '../proiektua/1_memoria/11_proposatutako_sistemaren_deskribapena.pdf'],
+	        [' 12 Arrisku analisia', '../proiektua/1_memoria/12_arrisku_analisia.pdf'],
+	        [' 13 Proiektuaren antolaketa eta kudeaketa', '../proiektua/1_memoria/13_proiektuaren_antolaketa_kudeaketa.pdf'],
+	        [' 14 Denbora planifikazioa', '../proiektua/1_memoria/14_denbora_planifikazioa.pdf'],
+	        [' 15 Aurrekontuaren laburpena', '../proiektua/1_memoria/15_aurrekontua.pdf'],
+			[' 16 Oinarrizko dokumentuen ordena', '../proiektua/1_memoria/16_dokumentu_ordena.pdf']
+	    ],
+
+		[' Memoriaren eranskinak','../proiektua/2_eranskinak/1_memoriaren_eranskinak/0_memoriaren_eranskinak.pdf',
+			[' A1 - Sarrerako Dokumentazioa','null',
+				[' Bezeroen eskakizun ofizialak', '../Proyecto/Memoria/1_0_memoria_archivos/bezeroaren_eskakizun_ofizialak.pdf'],
+				[' Enpresaren informazioa', '../Proyecto/Memoria/1_0_memoria_archivos/enpresaren_informazioa.pdf'],
+			],
+			[' A2 - Analisi eta Diseinua','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A2_analisi_diseinua/0_A2_analisi_diseinua.pdf',
+				[' Arkitektura','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A2_analisi_diseinua/1_arkitektura/0_arkitektura.pdf',
+					[' Arkitektura Koadernoa I','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A2_analisi_diseinua/1_arkitektura/1_arkitektura_koadernoa_1.pdf'],
+					[' Arkitektura Koadernoa II','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A2_analisi_diseinua/1_arkitektura/2_arkitektura_koadernoa_2.pdf']
+				]
+			],
+			[' A3 - Tamaina eta Esfortzu Estimazioa','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A3_tamaina_esfortzu_estimazioa/0_A3_tamaina_esfortzu_estimazioak.pdf'],
+			[' A4 - Kudeaketa Plana','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A4_kudeaketa_plana/0_kudeaketa_plana.pdf',
+				[' Integrazioaren Kudeaketa','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A4_kudeaketa_plana/1_integrazio_kudeaketa.pdf'],
+				[' Irismenaren Kudeaketa','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A4_kudeaketa_plana/2_irismen_kudeaketa.pdf'],
+				[' Epeen Kudeaketa','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A4_kudeaketa_plana/3_epeen_kudeaketa.pdf'],
+				[' Produktuaren Kostuen Kudeaketa','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A4_kudeaketa_plana/4_kostuen_kudeaketa.pdf'],
+				[' Kalitate Kudeaketa','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A4_kudeaketa_plana/5_kalitatearen_kudeaketa.pdf'],
+				[' Giza Baliabideen Kudeaketa','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A4_kudeaketa_plana/6_giza_baliabideen_kudeaketa.pdf'],
+				[' Komunikazioen Kudeaketa','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A4_kudeaketa_plana/7_komunikazioen_kudeaketa.pdf'],
+				[' Arriskuen Kudeaketa','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A4_kudeaketa_plana/8_arriskuen_kudeaketa.pdf'],
+				[' Erosketen Kudeaketa','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A4_kudeaketa_plana/9_erosketen_kudeaketa.pdf'],
+				[' Interesatuen Kudeaketa','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A4_kudeaketa_plana/10_interesatuen_kudeaketa.pdf'],
+			],
+			[' A5 - Segurtasun Plana','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A5_segurtasun_plana/0_A5_segurtasuna.pdf'],
+			[' A6 - Beste Eranskinak','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A6_beste_eranskinak/0_A6_beste_eranskinak.pdf',
+				[' Hedapena','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A6_beste_eranskinak/1_hedapena/0_hedapena.pdf',
+					[' Produktuaren dokumentazioa','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A6_beste_eranskinak/1_hedapena/1_produktuaren_dokumentazioa.pdf'],
+					[' Erabiltzaile dokumentazioa - I','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A6_beste_eranskinak/1_hedapena/2_erabiltzaile_dokumentazioa_1.pdf'],
+					[' Erabiltzaile dokumentazioa - II','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A6_beste_eranskinak/1_hedapena/2_erabiltzaile_dokumentazioa_2.pdf'],
+					[' Backout Plana','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A6_beste_eranskinak/1_hedapena/3_backout_plana.pdf'],
+					[' Hedapen Plana','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A6_beste_eranskinak/1_hedapena/4_hedapen_plana.pdf'],
+					[' Azpiegitura','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A6_beste_eranskinak/1_hedapena/5_azpiegitura.pdf']
+				],
+				[' Garapena','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A6_beste_eranskinak/2_garapena/0_garapena.pdf',
+					[' Build','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A6_beste_eranskinak/2_garapena/2_build/0_build.pdf'],
+					[' Build - ZIP Folder','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A6_beste_eranskinak/2_garapena/2_build/1_build.zip'],
+					[' Garatzaileen Probak','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A6_beste_eranskinak/2_garapena/3_garatzaileen_probak.pdf'],
+					[' Diseinua - I','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A6_beste_eranskinak/2_garapena/4_diseinua_uc1.pdf'],
+					[' Diseinua - II','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A6_beste_eranskinak/2_garapena/4_diseinua_uc2.pdf']
+				],
+				[' Ingurunea','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A6_beste_eranskinak/3_ingurunea/0_ingurunea.pdf',
+					[' Proiektuak Definitutako Prozesua','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A6_beste_eranskinak/3_ingurunea/1_proiektuak_definitutako_prozesua.pdf'],
+					[' Tresnak','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A6_beste_eranskinak/3_ingurunea/2_tresnak.pdf'],
+					[' Garapen Kasua','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A6_beste_eranskinak/3_ingurunea/3_garapen_kasua.pdf']
+				],
+				[' Inplementazioa','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A6_beste_eranskinak/4_inplementazioa/0_inplementazioa.pdf',
+					[' Inplementazioa - I','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A6_beste_eranskinak/4_inplementazioa/1_inplementazioa_1.pdf'],
+					[' Inplementazioa - II','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A6_beste_eranskinak/4_inplementazioa/1_inplementazioa_2.pdf'],
+					[' DocGenerator','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A6_beste_eranskinak/4_inplementazioa/DocGeneratorFullPackage.zip'],
+					[' RequirementTracer', '../proiektua/2_eranskinak/1_memoriaren_eranskinak/A6_beste_eranskinak/4_inplementazioa/RequirementTracerFullPackage.zip']
+				],
+				[' Test','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A6_beste_eranskinak/5_test/0_probak.pdf',
+					[' Proba Kasuak','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A6_beste_eranskinak/5_test/1_proba_kasuak.pdf'],
+					[' Proba Log-a','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A6_beste_eranskinak/5_test/2_proba_loga.pdf'],
+					[' Proba Script-a','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A6_beste_eranskinak/5_test/3_proba_scripta.pdf']
+				]
+			]
+		],
+
+		[' Sistemaren Espezifikazioa','../proiektua/2_eranskinak/2_sistemaren_espezifikazioa/0_sistemaren_espezifikazioak.pdf',
+			[' Glosarioa','../Proyecto/Memoria/1_0_memoria_archivos/rup_glossary.pdf'],
+			[' Bisioa','../Proyecto/Memoria/1_0_memoria_archivos/vision_tpl.pdf'],
+			[' Betebeharren Espezifikazioa','../Proyecto/Memoria/1_0_memoria_archivos/systemwide_req_spec.pdf'],
+			[' Erabilpen-Kasuak', null,
 								['01. Erregistroa', '../Proyecto/Memoria/1_0_memoria_archivos/usecase_specification_tpl_register.pdf'],
 								['02. Login', '../Proyecto/Memoria/1_0_memoria_archivos/usecase_specification_tpl_login.pdf'],
 								['03. Zientifikoei rola/baimena eman', '../Proyecto/Memoria/1_0_memoria_archivos/usecase_specification_tpl_zientifiko_rola_eman.pdf'],
@@ -25,15 +98,49 @@ var TREE_ITEMS =
 								['05. Gida klinikoak ikusi', '../Proyecto/Memoria/1_0_memoria_archivos/usecase_specification_tpl_gida_klinikoak_ikusi.pdf'],
 								['06. Diagnostikoa lortu', '../Proyecto/Memoria/1_0_memoria_archivos/usecase_specification_tpl_diagnostikoa_lortu.pdf'],
 							],
-						]
-				],
-				['Sistemaren analisia eta diseinua', '../Proyecto/Memoria-Anexos/4_analisis-diseno.htm',
-				         ['Softwarearen arkitekturaren dokumentazioa', '../Proyecto/Memoria-Anexos/Analisis y diseno del sistema/Documento de arquitectura de software.html'],
-				         ['Analisi eredua', '../Proyecto/Memoria-Anexos/Analisis y diseno del sistema/Modelo de analisis.html'],
-				         ['Erabiltzaileen interfazearen prototipoa', '../Proyecto/Memoria-Anexos/Analisis y diseno del sistema/Prototipo de interfaz de usuario.html'],
-				         ['Diseinu eredua ', '../Proyecto/Memoria-Anexos/Analisis y diseno del sistema/Modelo de diseno.html'],
-				         ['Ezarpen eredua', '../Proyecto/Memoria-Anexos/Analisis y diseno del sistema/Modelo de despliegue.html']
-                                ],
-					],
-				],                         
-			];
+			[' Erabilpen Kasuen Eredua','../proiektua/2_eranskinak/2_sistemaren_espezifikazioa/4_erabilpen_kasuen_eredua.pdf']
+		],
+		
+		[' Aurrekontua','../proiektua/2_eranskinak/3_aurrekontua/0_aurrekontua.pdf',
+			[' Orokortasunak', '../proiektua/2_eranskinak/3_aurrekontua/1_orokortasunak.pdf'],
+			[' Edukia', '../proiektua/2_eranskinak/3_aurrekontua/2_edukia.pdf']
+		],
+
+		[' Ikerlanak','../proiektua/2_eranskinak/4_ikerlanak/0_ikerlanak.pdf',
+			[' Orokortasunak', '../proiektua/2_eranskinak/4_ikerlanak/1_orokortasunak.pdf'],
+			[' Edukia', '../proiektua/2_eranskinak/4_ikerlanak/2_edukia.pdf']
+		]
+	],
+
+	['KlinikApp: Barne Kudeaketa', '../proiektua/3_barne_kudeaketa.htm',
+		[' Proiektuaren Barne Kudeaketa','../proiektua/3_proiektuaren_kudeaketa/0_proiektuaren_kudeaketa.pdf',
+			[' Iterazio Plana','../proiektua/3_proiektuaren_kudeaketa/1_iterazio_plana.pdf'],
+			[' Proiektu Plana','../proiektua/3_proiektuaren_kudeaketa/2_proiektu_plana.pdf'],
+			[' Arrisku Zerrenda','../proiektua/3_proiektuaren_kudeaketa/3_arrisku_zerrenda.pdf'],
+			[' Lan-Atazen Zerrenda','../proiektua/3_proiektuaren_kudeaketa/4_lan_atazen_zerrenda.pdf'],
+			[' Barne Kudeaketa','../proiektua/3_proiektuaren_kudeaketa/5_barne_kudeaketa.pdf'],
+			[' Trebatzeko Materialak','../proiektua/3_proiektuaren_kudeaketa/6_trebatzeko_materialak.pdf'],
+			[' Hizkuntza Hitzarmena','../proiektua/3_proiektuaren_kudeaketa/7_hizkuntza_hitzarmena.pdf'],
+				[' Bilera Aktak', '../proiektua/3_proiektuaren_kudeaketa/8_aktak/0_aktak.pdf',
+					['2019.02.06','../proiektua/3_proiektuaren_kudeaketa/8_aktak/akta_20190206.pdf'],
+					['2019.02.13','../proiektua/3_proiektuaren_kudeaketa/8_aktak/akta_20190213.pdf'],
+					['2019.02.20','../proiektua/3_proiektuaren_kudeaketa/8_aktak/akta_20190220.pdf'],
+					['2019.03.06 - I','../proiektua/3_proiektuaren_kudeaketa/8_aktak/akta_20190306.pdf'],
+					['2019.03.06 - II','../proiektua/3_proiektuaren_kudeaketa/8_aktak/akta_20190306_2.pdf'],
+					['2019.03.20','../proiektua/3_proiektuaren_kudeaketa/8_aktak/akta_20190320.pdf'],
+					['2019.04.02','../proiektua/3_proiektuaren_kudeaketa/8_aktak/akta_20190402.pdf'],
+					['2019.04.03','../proiektua/3_proiektuaren_kudeaketa/8_aktak/akta_20190403.pdf'],
+					['2019.04.11','../proiektua/3_proiektuaren_kudeaketa/8_aktak/akta_20190411.pdf'],
+					['2019.04.23 - I','../proiektua/3_proiektuaren_kudeaketa/8_aktak/akta_20190423_1.pdf'],
+					['2019.04.23 - II','../proiektua/3_proiektuaren_kudeaketa/8_aktak/akta_20190423_2.pdf'],
+					['2019.05.14 - I','../proiektua/3_proiektuaren_kudeaketa/8_aktak/akta_20190514_1.pdf'],
+					['2019.05.14 - II','../proiektua/3_proiektuaren_kudeaketa/8_aktak/akta_20190514_2.pdf'],
+					['2019.05.22 - I','../proiektua/3_proiektuaren_kudeaketa/8_aktak/akta_20190522_1.pdf'],
+					['2019.05.22 - II','../proiektua/3_proiektuaren_kudeaketa/8_aktak/akta_20190522_2.pdf'],
+					['2019.06.12','../proiektua/3_proiektuaren_kudeaketa/8_aktak/akta_20190612.pdf'],
+					['2019.06.17','../proiektua/3_proiektuaren_kudeaketa/8_aktak/akta_20190617.pdf'],
+					['2019.07.04','../proiektua/3_proiektuaren_kudeaketa/8_aktak/akta_20190704.pdf']
+				]
+		]
+	]					
+];

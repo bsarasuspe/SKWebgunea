9b32ebced394782b3f56f80f401f1e350a385fdc
bsarasuspe <83415989+bsarasuspe@users.noreply.github.com>
2022-03-06 15:49:40 +0100

    Erabilpen kasu eredua + analisi eredua

--- menu/tree_items.js
+++ menu/tree_items.js
@@ -35,7 +35,8 @@ var TREE_ITEMS = [
 				[' Arkitektura','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A2_analisi_diseinua/1_arkitektura/0_arkitektura.pdf',
 					[' Arkitektura Koadernoa I','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A2_analisi_diseinua/1_arkitektura/1_arkitektura_koadernoa_1.pdf'],
 					[' Arkitektura Koadernoa II','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A2_analisi_diseinua/1_arkitektura/2_arkitektura_koadernoa_2.pdf']
-				]
+				],
+				[' Analisi eredua','../Proyecto/Memoria/1_0_memoria_archivos/modelo-analisis-rup/index.html']
 			],
 			[' A3 - Tamaina eta Esfortzu Estimazioa','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A3_tamaina_esfortzu_estimazioa/0_A3_tamaina_esfortzu_estimazioak.pdf'],
 			[' A4 - Kudeaketa Plana','../proiektua/2_eranskinak/1_memoriaren_eranskinak/A4_kudeaketa_plana/0_kudeaketa_plana.pdf',
@@ -98,7 +99,7 @@ var TREE_ITEMS = [
 								['05. Gida klinikoak ikusi', '../Proyecto/Memoria/1_0_memoria_archivos/usecase_specification_tpl_gida_klinikoak_ikusi.pdf'],
 								['06. Diagnostikoa lortu', '../Proyecto/Memoria/1_0_memoria_archivos/usecase_specification_tpl_diagnostikoa_lortu.pdf'],
 							],
-			[' Erabilpen Kasuen Eredua','../proiektua/2_eranskinak/2_sistemaren_espezifikazioa/4_erabilpen_kasuen_eredua.pdf']
+			[' Erabilpen Kasuen Eredua','../Proyecto/Memoria/1_0_memoria_archivos/modelo-casos-uso/index.html']
 		],
 		
 		[' Aurrekontua','../proiektua/2_eranskinak/3_aurrekontua/0_aurrekontua.pdf',
